585b1dfd7cfb89f2458ad213d5bf2e26d7cc2271
walemust <walemust28@gmail.com>
2023-01-11 15:10:54 +0100

    implemented the loop for the slider

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  public image: string =
+    'https://media.premiumtimesng.com/wp-content/files/2022/02/Dbanj.jpg';
   constructor() {}
-
 }
