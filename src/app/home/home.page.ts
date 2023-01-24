7c4daef91c0411cb2bed15153b6e609e91fe92b8
walemust <walemust28@gmail.com>
2023-01-24 12:09:21 +0100

    slider update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Product } from '../product';
+import { IonSlides } from '@ionic/angular';
 
 //import * as WC from 'woocommerce-api';
 import { RemoteServiceService } from './../remote-service.service';
@@ -12,6 +13,12 @@ import { RemoteServiceService } from './../remote-service.service';
 export class HomePage {
   WooCommerce: any;
   products: Product[] = [];
+
+  productSlides!: {
+    initialSlide: 0;
+    slidesPerView: 1;
+    autoplay: true;
+  };
 
   public image: string =
     'https://media.premiumtimesng.com/wp-content/files/2022/02/Dbanj.jpg';
@@ -29,4 +36,16 @@ export class HomePage {
       },
     });
   }
+
+  // ionViewDidLoad() {
+  //   setInterval(async () => {
+  //     if (
+  //       (await this.productSlides.getActiveIndex()) ==
+  //       (await this.productSlides.length()) - 1
+  //     )
+  //       this.productSlides.slideTo(0);
+
+  //     this.productSlides.slideNext();
+  //   }, 3000);
+  // }
 }
